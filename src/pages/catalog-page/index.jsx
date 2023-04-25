4ab4903339d10f87376a4cad12af7e5660b439c9
НАТАЛИ <natalya.osetrova1980@gmail.com>
2023-04-25 09:38:17 +0300

    добавляет карусель

--- src/pages/catalog-page/index.jsx
+++ src/pages/catalog-page/index.jsx
@@ -7,14 +7,18 @@ import s from './styles.module.css';
 import { CardsContext } from '../../contexts/card-context';
 import { ContentHeader } from '../../components/content-header';
 import { TABS } from '../../utils/constants';
+import Carousel from '../../components/carousel';
+import { Card } from '../../components/card';
 
 export const CatalogPage = () => {
     const { cards: goods } = useContext(CardsContext)
     return (
         <>
+            
             <ContentHeader title="Каталог" textButton="Главная" to="/" />
             <Sort tabs={TABS} currentSort="discount" onChangeSort={(data) => console.log(data)} />
             <CardList goods={goods} />
+            <Carousel items={goods} component={Card} perView={4} />
         </>
     )
 }
