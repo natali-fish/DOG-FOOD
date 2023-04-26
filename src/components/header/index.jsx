16b6ba119737879ad3d9dd88d8955a1952164e6f
НАТАЛИ <natalya.osetrova1980@gmail.com>
2023-04-26 18:59:44 +0300

    добавляет redux

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,23 +1,27 @@
 import cn from 'classnames';
 import { useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
 import { UserContext } from '../../contexts/current-user-context';
-import { Button } from '../button';
+import { ThemeContext } from '../../contexts/theme-context';
+import { CardsContext } from '../../contexts/card-context';
+import { ReactComponent as FavoriteIcon } from './img/favorites.svg'
+import { ReactComponent as LogoutIcon } from './img/logout.svg';
+import { ReactComponent as CartIcon } from './img/cart.svg';
+import { ReactComponent as ProfileIcon } from './img/profile.svg';
+import { ReactComponent as UserIcon } from './img/user.svg';
 
 import s from "./styles.module.css";
 import "./styles.css";
-import { ThemeContext } from '../../contexts/theme-context';
-import { CardsContext } from '../../contexts/card-context';
-import { Link, useLocation } from 'react-router-dom';
-import { ReactComponent as FavoriteIcon } from './img/favorites.svg'
-
+import { useSelector } from 'react-redux';
 
 export function Header({ children }) {
-  const { currentUser, onUpdateUser } = useContext(UserContext);
-  const { favorites } = useContext(CardsContext)
+  const currentUser = useSelector(state => state.user.data);
+  const favorites = useSelector(state => state.products.favoriteProducts)
   const { toggleTheme } = useContext(ThemeContext)
   const location = useLocation()
   const handleClickButtonEdit = () => {
-    onUpdateUser({ name: 'Наталья', about: 'Студент' })
+    // onUpdateUser({ name: 'Наталья', about: 'студент' })
   }
 
   return (
@@ -29,10 +33,30 @@ export function Header({ children }) {
             <FavoriteIcon />
             {favorites.length !== 0 && <span className={s.iconBubble}>{favorites.length}</span>}
           </Link>
-          <Link to='/login' replace state={{ backgroundLocation: location, initialPath: location.pathname }}>Войти</Link>
+
+          <Link className={s.favoritesLink} to={{ pathname: '/cart' }}>
+            <CartIcon />
+            {favorites.length !== 0 && <span className={s.iconBubble}>{favorites.length}</span>}
+          </Link>
+
+          <Link to='/login' className={s.iconsMenuItem} replace state={{ backgroundLocation: location, initialPath: location.pathname }}>
+            <UserIcon />
+            Войти
+          </Link>
+
+
+          <Link to='/profile' className={s.iconsMenuItem}>
+            <ProfileIcon />
+            Наталья
+          </Link>
+
+          <Link to='/' className={s.iconsMenuItem}>
+            <LogoutIcon />
+            Выйти
+          </Link>
+
         </div>
-
-
+     
 
 
 
--- src/pages/catalog-page/index.jsx
+++ src/pages/catalog-page/index.jsx
@@ -1,24 +1,20 @@
-import { useContext } from 'react';
 import { CardList } from '../../components/card-list'
 import { Sort } from '../../components/sort'
-import { Spinner } from '../../components/spinner';
 
-import s from './styles.module.css';
-import { CardsContext } from '../../contexts/card-context';
 import { ContentHeader } from '../../components/content-header';
 import { TABS } from '../../utils/constants';
-import Carousel from '../../components/carousel';
-import { Card } from '../../components/card';
+import { useSelector } from 'react-redux';
 
 export const CatalogPage = () => {
-    const { cards: goods } = useContext(CardsContext)
+    const goods = useSelector(state => state.products.data)
+
     return (
         <>
             
             <ContentHeader title="Каталог" textButton="Главная" to="/" />
             <Sort tabs={TABS} currentSort="discount" onChangeSort={(data) => console.log(data)} />
             <CardList goods={goods} />
-            <Carousel items={goods} component={Card} perView={4} />
+            {/* <Carousel items={goods} component={Card} perView={4} /> */}
         </>
     )
 }
